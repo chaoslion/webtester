7e1d0afee25068bd14812af4bbc5a246af9c7068
Alexander Jähnel <trafo1@gmx.de>
2016-09-11 14:38:27 +0200

    first commit

---
+++ webtester.sh
@@ -0,0 +1,26 @@
+#/bin/sh
+
+if [ "$#" -ne 1 ]; then
+	echo "Webaddress expected"
+	exit
+fi
+
+TEST_TOTAL=10
+TTFB_ACC=0
+TOTAL_ACC=0
+echo "Testing Address '$1' over $TEST_TOTAL iterations"
+for i in $(seq 1 $TEST_TOTAL);
+do
+	RESULT=$(curl -s -o /dev/null -w "%{time_connect} %{time_starttransfer} %{time_total} \n" $1 | tr ',' '.')
+	TTFB=$(echo $RESULT | awk '{print $2}')
+	TOTAL=$(echo $RESULT | awk '{print $3}')
+	TTFB_ACC=$(echo "$TTFB_ACC+$TTFB" | bc)
+	TOTAL_ACC=$(echo "$TOTAL_ACC+$TOTAL" | bc)
+
+	#echo $TTFB
+	echo "iteration $i: $TOTAL"
+done
+TTFB=$(echo "scale=4;$TTFB_ACC/$TEST_TOTAL" | bc)
+TOTAL=$(echo "scale=4;$TOTAL_ACC/$TEST_TOTAL" | bc)
+echo "TTFB: $TTFB"
+echo "TOTAL: $TOTAL"
