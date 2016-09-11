ae9b7d562763fcaa7a27e4d013d248fac78b3364
Alexander Jähnel <trafo1@gmx.de>
2016-09-11 14:41:40 +0200

    made iterations a parameter

--- webtester.sh
+++ webtester.sh
@@ -1,17 +1,18 @@
 #/bin/sh
 
-if [ "$#" -ne 1 ]; then
-	echo "Webaddress expected"
+if [ "$#" -ne 2 ]; then
+	echo "address and number of iterations expected"
 	exit
 fi
 
-TEST_TOTAL=10
+TEST_ADDRESS=$1
+TEST_TOTAL=$2
 TTFB_ACC=0
 TOTAL_ACC=0
 echo "Testing Address '$1' over $TEST_TOTAL iterations"
 for i in $(seq 1 $TEST_TOTAL);
 do
-	RESULT=$(curl -s -o /dev/null -w "%{time_connect} %{time_starttransfer} %{time_total} \n" $1 | tr ',' '.')
+	RESULT=$(curl -s -o /dev/null -w "%{time_connect} %{time_starttransfer} %{time_total} \n" $TEST_ADDRESS | tr ',' '.')
 	TTFB=$(echo $RESULT | awk '{print $2}')
 	TOTAL=$(echo $RESULT | awk '{print $3}')
 	TTFB_ACC=$(echo "$TTFB_ACC+$TTFB" | bc)
